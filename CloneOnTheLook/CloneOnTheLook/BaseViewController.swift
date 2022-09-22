435d337621d9b5b598dd698c0d2230d6412ee7cc
Grumpy <sewoong2890@gmail.com>
2022-09-22 22:40:12 +0900

    chore: 프로젝트 초기 설정

---
+++ CloneOnTheLook/CloneOnTheLook/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  CloneOnTheLook
+//
+//  Created by 박세웅 on 2022/09/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
