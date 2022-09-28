bcc53776bbf08289cff62abe60771be5922fe318
Grumpy <sewoong2890@gmail.com>
2022-09-29 01:07:48 +0900

    feat: BaseCollectionViewCell에 id를 label 추가

--- CloneOnTheLook/CloneOnTheLook/BaseCollectionViewCell.swift
+++ CloneOnTheLook/CloneOnTheLook/BaseCollectionViewCell.swift
@@ -23,6 +23,14 @@ final class BaseCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+//    override var isSelected: Bool {
+//        didSet {
+//            if isSelected {
+//                coverView.isHidden.toggle()
+//            }
+//        }
+//    }
+    
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -33,8 +41,34 @@ final class BaseCollectionViewCell: UICollectionViewCell {
         return imageView
     }()
     
+    let idLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 10, weight: .bold)
+        return label
+    }()
+    
+    let coverView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        view.layer.opacity = 0.5
+        view.isHidden = true
+        return view
+    }()
+    
+    let heartView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "suit.heart.fill"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     private func setSubViews() {
+        coverView.addSubview(heartView)
         contentView.addSubview(imageView)
+        contentView.addSubview(idLabel)
+        contentView.addSubview(coverView)
     }
     
     private func setLayoutConstraints() {
@@ -44,6 +78,30 @@ final class BaseCollectionViewCell: UICollectionViewCell {
             imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
+        
+        NSLayoutConstraint.activate([
+            idLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            idLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            coverView.topAnchor.constraint(equalTo: self.topAnchor),
+            coverView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            coverView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            coverView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            heartView.centerXAnchor.constraint(equalTo: coverView.centerXAnchor),
+            heartView.centerYAnchor.constraint(equalTo: coverView.centerYAnchor),
+            heartView.heightAnchor.constraint(equalToConstant: 30),
+            heartView.widthAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    func setCellData(_ cellInfo: BaseCellModel) {
+        imageView.image = cellInfo.image
+        idLabel.text = cellInfo.user
     }
 }
 
@@ -51,5 +109,7 @@ extension BaseCollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         imageView.image = nil
+        idLabel.text = nil
+        coverView.isHidden = true
     }
 }
